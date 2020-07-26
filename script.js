5f3a2fee2485fc9e4f9bcc86e70e88df487bd816
Cris Perez <cristino.perez525@gmail.com>
2020-07-25 21:08:26 -0500

    Defined all variables that come to mind for generating random password, set up prompts to gather user preferences for password generated, using console log method to make sure writing is working

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz".split("")
+var numberChar = "0123456789".split("")
+var specialChar = ("?!@#$%^&*()_+=").split("")
+var passLength = prompt("How many characters would you like? (max 20)?")
+var wantUpper = confirm("Would you like upper-case letters?")
+var wantLower = confirm("Would you like lower-case letters?")
+var wantNums = confirm("Would you like numbers?")
+var wantSpecial = confirm("Finally, would you like special characters?")
+
+// Generate password on click
+generateBtn.addEventListener("click", function() {
+
+  var randomChar = "";
+
+  for (i = 0; i < passLength; i++) {
+
+    randomChar += upperCaseChar[Math.floor(Math.random() * upperCaseChar.length)]
+  }
+
+  console.log(randomChar)
+})
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
