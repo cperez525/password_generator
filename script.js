37ebafb82b667213cf913af2b095e8ccb1501365
Cris Perez <cristino.perez525@gmail.com>
2020-07-26 12:17:11 -0500

    began writing out generatepassword function, conditionals to support it, and figuring out how to omit commas from final password

--- script.js
+++ script.js
@@ -3,26 +3,53 @@ var generateBtn = document.querySelector("#generate");
 var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz".split("")
 var numberChar = "0123456789".split("")
-var specialChar = ("?!@#$%^&*()_+=").split("")
-var passLength = prompt("How many characters would you like? (max 20)?")
+var specialChar = "?!@#$%^&*()_+=".split("")
+var passLength = prompt("How many characters would you like (Pick a number between 8 and 128)?")
 var wantUpper = confirm("Would you like upper-case letters?")
 var wantLower = confirm("Would you like lower-case letters?")
 var wantNums = confirm("Would you like numbers?")
 var wantSpecial = confirm("Finally, would you like special characters?")
 
-// Generate password on click
-generateBtn.addEventListener("click", function() {
+// Generate password 
+function generatePassword(){
 
-  var randomChar = "";
+  var passArray = ""
+  var randomPw = ""
 
-  for (i = 0; i < passLength; i++) {
-
-    randomChar += upperCaseChar[Math.floor(Math.random() * upperCaseChar.length)]
+  if (wantUpper) {
+    passArray += upperCaseChar
   }
 
-  console.log(randomChar)
-})
+  if (wantLower) {
+    passArray += lowerCaseChar
+  }
 
+  if (wantNums) {
+    passArray += numberChar
+  }
+
+  if (wantSpecial) {
+    passArray += specialChar
+  }
+
+  console.log(passArray)
+
+  if (wantUpper || wantLower || wantNums || wantSpecial) {
+
+    for (i = 0; i < passLength; i++){
+
+      randomPw += passArray[Math.floor(Math.random() * passArray.length)]
+    }
+    
+    console.log(randomPw)
+  }
+  
+
+}
+
+generatePassword()
+
+/*
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -34,4 +61,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); */
